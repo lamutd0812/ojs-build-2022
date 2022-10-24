a3c8fc30914208e6b4653b91fc92c9d6845a608c
Lam Nguyen <lamutd0812@gmail.com>
2022-10-24 18:24:52 +0700

    test

---
+++ build/precache-manifest.e3eceb14b80001fd47f49629419122e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c198b4f4096641a7d392b0c731c41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc86aa7c2d01a6656e0",
+    "url": "/static/css/2.c0e5dd81.chunk.css"
+  },
+  {
+    "revision": "21e09c64d0bd611a550f",
+    "url": "/static/css/main.f15c4942.chunk.css"
+  },
+  {
+    "revision": "3dc86aa7c2d01a6656e0",
+    "url": "/static/js/2.f5dc03ca.chunk.js"
+  },
+  {
+    "revision": "b839c824423fc12f25872778360f3407",
+    "url": "/static/js/2.f5dc03ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e09c64d0bd611a550f",
+    "url": "/static/js/main.5c61f2da.chunk.js"
+  },
+  {
+    "revision": "93a7db6d10474e04366f",
+    "url": "/static/js/runtime-main.1c09a160.js"
+  },
+  {
+    "revision": "dcfc6bbbf49b6406b82ab1b071b70d9a",
+    "url": "/static/media/40.dcfc6bbb.jpg"
+  }
+]);
